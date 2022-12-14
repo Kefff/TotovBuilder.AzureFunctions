cc09849c4ca233550f8f6fa2e8a3f7d9df6f5290
Kefff <1677411+Kefff@users.noreply.github.com>
2022-12-14 23:28:57 +0100

    Fixed Application Insights configuration.

--- src/TotovBuilder.AzureFunctions/Program.cs
+++ src/TotovBuilder.AzureFunctions/Program.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using TotovBuilder.AzureFunctions;
 using TotovBuilder.AzureFunctions.Abstractions;
 using TotovBuilder.AzureFunctions.Abstractions.Fetchers;
 using TotovBuilder.AzureFunctions.Fetchers;
 
 IHost host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWorkerDefaults(builder =>
+    {
+        // Application Insights configuration
+        // Cf https://github.com/Azure/azure-functions-dotnet-worker/issues/1182#issuecomment-1317230604
+        builder
+            .AddApplicationInsights()
+            .AddApplicationInsightsLogger();
+    })
     .ConfigureServices((HostBuilderContext hopstBuilderContext, IServiceCollection serviceCollection) =>
     {
-        serviceCollection.AddApplicationInsightsTelemetryWorkerService();
-
         serviceCollection.AddHttpClient();
 
         serviceCollection.AddSingleton<IArmorPenetrationsFetcher, ArmorPenetrationsFetcher>();
@@ -30,6 +39,28 @@ IHost host = new HostBuilder()
         serviceCollection.AddSingleton<IPricesFetcher, PricesFetcher>();
         serviceCollection.AddSingleton<ITarkovValuesFetcher, TarkovValuesFetcher>();
         serviceCollection.AddSingleton<IWebsiteConfigurationFetcher, WebsiteConfigurationFetcher>();
+
+        serviceCollection.Configure<LoggerFilterOptions>(options =>
+        {
+            // Removing default filters that only allow warnings and errors to be logged
+            LoggerFilterRule? filtersToRemove = options.Rules.FirstOrDefault(rule =>
+                rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+            if (filtersToRemove != null)
+            {
+                options.Rules.Remove(filtersToRemove);
+            }
+        });
+    })
+    .ConfigureAppConfiguration((hostContext, config) =>
+    {
+        // Configuring the application using appsettings.json file
+        config.AddJsonFile("appsettings.json", optional: true);
+    })
+    .ConfigureLogging((hostingContext, logging) =>
+    {
+        // Making sure the configuration of the appsettings.json file is used
+        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
     })
     .Build();
 
